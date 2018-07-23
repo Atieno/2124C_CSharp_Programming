b8bf1beee456ec9a5d3af454c1d23489d3fafb00
Daniel Atieno <danoluoch@gmail.com>
2018-07-23 18:48:34 +0200

    Add files via upload

---
+++ TimeProg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time now = new Time();
+            now.hour = 11;
+            now.minute = 59; 
+            Console.WriteLine("The time now is "+now.hour+":"+ now.minute);
+            
+        }
+    }
+}
